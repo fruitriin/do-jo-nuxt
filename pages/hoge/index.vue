312654bc338b9a9e71a7351ad105566766df7029
FruitRiin <nassii74@gmail.com>
2023-11-09 22:00:21 +0900

    addなんかいろいろ

---
+++ pages/hoge/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>hoge index</div>
+</template>
+
+<script >
+export default {
+
+}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
   <div>index</div>
+
+  <input type="text" v-model="input.username">
+  <input type="text" v-model="input.password">
+
+  <p>{{ loginAble() }}</p>
+  <button :disabled="!loginAble()" @click="login">ろぐいん！</button>
+
 </template>
 
 <script >
 export default {
+  data(){
+    return {
+      input: {
+        username: "",
+        password: "",
+      },
+      test: "makimasu"
+    }
+  },
+  methods: {
+    loginAble(){
+      if(this.input.username !== "user") return false
+      if(this.input.password !== "password") return false
+
+      return true
+
+    },
+    login(){
+      if(this.input.username !== "user") return false
+      if(this.input.password !== "password") return false
+
+      this.$router.push("/bbbb")
+    }
+  }
 
 }
 
